bef51dbf2a5a0642ba4184d89adba993a92082ad
FARABI12344 <farabiehan4@gmail.com>
2025-05-04 22:36:20 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        pinky: '#ffd1dc',
+        purply: '#4b0082'
+      }
+    }
+  },
+  plugins: [],
+}
